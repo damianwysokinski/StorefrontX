e9817d2424d9eaadb185001bcdcf4410718f832a
damianwysokinski <damian.wysokinski99@gmail.com>
2025-03-05 17:48:22 +0100

    refactor formatDate function

--- apps/web/src/lib/utils/format-date.ts
+++ apps/web/src/lib/utils/format-date.ts
@@ -1,31 +1,8 @@
 export const formatDate = (isoDateString: string): string => {
-  const date = new Date(isoDateString)
-
-  const months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ]
-
-  const month = months[date.getMonth()]
-  const day = date.getDate()
-  let hours = date.getHours()
-  const minutes = date.getMinutes()
-  const ampm = hours >= 12 ? 'pm' : 'am'
-
-  hours = hours % 12
-  hours = hours ? hours : 12
-
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes
-
-  return `${month} ${day} at ${hours}:${formattedMinutes} ${ampm}`
+  return new Date(isoDateString).toLocaleDateString('en-US', {
+    day: 'numeric',
+    month: 'short',
+    hour: 'numeric',
+    minute: 'numeric',
+  })
 }
