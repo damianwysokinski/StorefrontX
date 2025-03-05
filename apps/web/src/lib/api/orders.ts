43053ca85ada29f04e290d6bb2b7cfb4d253b806
damianwysokinski <damian.wysokinski99@gmail.com>
2025-03-05 16:04:37 +0100

    create getOrderById action

--- apps/web/src/lib/api/orders.ts
+++ apps/web/src/lib/api/orders.ts
@@ -1,10 +1,16 @@
 'use server'
 
 import axios from 'axios'
+import { Order } from '@/types/order'
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL
 
 export async function getOrders() {
   const { data } = await axios.get(`${API_BASE_URL}/orders`)
+  return data
+}
+
+export async function getOrderById(id: string): Promise<Order> {
+  const { data } = await axios.get(`${API_BASE_URL}/orders/${id}`)
   return data
 }
