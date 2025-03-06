f3b09927fe0636cd21cc2d6bcbd69d51575bfee9
damianwysokinski <damian.wysokinski99@gmail.com>
2025-03-06 18:55:27 +0100

    remove session implementation

--- apps/web/src/actions/auth-actions.ts
+++ apps/web/src/actions/auth-actions.ts
@@ -1,25 +1,10 @@
 'use server'
 
-import { logInUser } from '@/lib/auth/auth'
-import { createSession } from '@/lib/auth/session'
 import { redirect } from 'next/navigation'
 
 export async function logIn(formData: FormData) {
   const email = formData.get('email') as string
   const password = formData.get('password') as string
-
-  const { user, accessToken, refreshToken } = await logInUser({
-    email,
-    password,
-  })
-
-  await createSession({
-    user: {
-      email: user.email,
-    },
-    accessToken,
-    refreshToken,
-  })
 
   redirect('/dashboard')
 }
