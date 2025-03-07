087ccb70c78fc093140b115f3bbf0442983f772d
damianwysokinski <damian.wysokinski99@gmail.com>
2025-03-07 13:37:07 +0100

    add condition to check if no products in ProductsList

--- apps/web/src/app/(storefront)/_components/products/product-list.tsx
+++ apps/web/src/app/(storefront)/_components/products/product-list.tsx
@@ -7,13 +7,13 @@ interface ProductsListProps {
 }
 
 export default function ProductsList({ title, products }: ProductsListProps) {
+  if (!products.length) return
+
   return (
     <>
       {title && <h2 className="mb-4 text-xl font-bold">{title}</h2>}
       <ul className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {products.map((product: Product) => {
-          if (product.status === 'draft') return
-
           return (
             <li key={product.id}>
               <ProductCard {...product} />
